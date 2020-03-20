1ba40d2796561b68b441d885edc7a14f8823e416
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-20 19:36:34 -0400

    still trying

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -10,7 +11,16 @@ export default function CharacterList() {
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>
+        {data.map(character) => (
+          <CharacterCard
+            key={character.id}
+            image={character.image}
+            name={character.name}
+            species={character.species}
+            status={character.status}
+        )};
+      </h2>
     </section>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 export default function LocationsList() {}
+
+<Link to="/">
+  <button> Home</button>
+</Link>
+
+<Link to="/Characters">
+  <button>Characters</button>
+</Link>
+{DataCue.map((data,i)=> (
+  <LocationCard data={data} key {i}/>
+))};
