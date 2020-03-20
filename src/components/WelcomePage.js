b6a22364e9356d1b1dee20304f948c20a536634a
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-20 19:23:20 -0400

    some changes

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -5,6 +5,20 @@ export default function WelcomePage() {
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
+
+               <Nav>
+             <Link to="/Characters">              <Button variant="outlined" color="primary"> Characters
+            </Button>	           
+          </Link>	              
+          <Link to="/Search">	           
+            <Button variant="outline" color="primary"> Search	
+            </Button>            
+          </Link>	             
+          <Link to="/Locations">	           
+            <Button variant="outlined" color="primary">	Locations            
+          </Link>
+          </Nav>
+
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
