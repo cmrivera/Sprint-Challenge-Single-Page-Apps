eaaf4889e9bcd1859647f12325777998fedf492f
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-20 19:27:11 -0400

    trying to remember to commit

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  return (
+    <Card>
+      <img src={props.image} alt="character image" />
+      <h2> Name: {props.name}</h2>
+      <p> Species: {props.species}</p>
+      <p>Status: {props.status}</p>
+    </Card>
+  );
 }
