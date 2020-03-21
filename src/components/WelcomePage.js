d2393fc60de4c9a011f56ded98d6cdf82e823e44
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-20 20:08:51 -0400

    almost there

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function WelcomePage() {
   const Page = styled.div`
@@ -15,17 +15,18 @@ export default function WelcomePage() {
     margin-left: 15%;
   `;
   return (
-    <section className="welcome-page">	    
-      <header>	     
-        <h1>Welcome to the ultimate fan site!</h1>	        
-      
-      <Link to="/Characters">
-      <button>Characters</button>
-              </Link>
+    <Page>
+      <section className="welcome-page">
+        <header>
+          <h1>Welcome to the ultimate fan site!</h1>
 
-              <Link to="/Search">
-                <button>Search</button>
-              </Link>
+          <Link to="/Characters">
+            <button>Characters</button>
+          </Link>
+
+          <Link to="/Search">
+            <button>Search</button>
+          </Link>
           <Img
             className="main-img"
             src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
@@ -33,7 +34,6 @@ export default function WelcomePage() {
           />
         </header>
       </section>
-  
-  </Page>
-  )	  };
-
+    </Page>
+  );
+}
